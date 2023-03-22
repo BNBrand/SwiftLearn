35e17d6cd580c03b4d9c76f3367e98bf0ec863e6
BNBrand <bensaindifonbrandon@gmail.com>
2023-03-22 18:06:29 +0100

    commit_2_3/22/23

--- lib/screens/more/profile/profile_screen.dart
+++ lib/screens/more/profile/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:swift_learn/models/user_model.dart';
 
@@ -18,16 +19,19 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
 
   buildProfileButton(){
-    return Text('Profile button');
+    return const Text('Profile button');
   }
 
   buildCountColumn(String label, int count){
     return Column(
       children: [
-        Text(count.toString()),
+        Text(count.toString(),
+        style: const TextStyle(fontSize: 20,
+        fontWeight: FontWeight.w600
+        ),
+        ),
         Container(
-          child: Text(label,
-          ),
+          child: Text(label),
         )
       ],
     );
@@ -43,98 +47,115 @@ class _ProfileScreenState extends State<ProfileScreen> {
         Users user = Users.fromDocument(snapshot.data!);
         return Column(
           children: [
-            Stack(
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              fit: StackFit.loose,
-              alignment: Alignment.center,
-              children: [
-                Container(
-                  height: 150.0,
-                  decoration: const BoxDecoration(
-                      color: secondaryBackgroundColor,
-                      borderRadius: BorderRadius.only(
-                          bottomRight: Radius.circular(100),
-                          bottomLeft: Radius.circular(100)
-                      )
-                  ),
-                ),
-              Positioned(
-                bottom: 10.0,
-                child: Column(
-                  children: [
-                    Text(user.displayName),
-                    CircleAvatar(
-                      radius: 40.0,
-                      backgroundImage: CachedNetworkImageProvider(user.photoURL),
-                    ),
-                    Text(user.email)
-                  ],
-                ),
-              ),
-              ]
-            ),
             Container(
-              clipBehavior: Clip.hardEdge,
-              height: 70,
-              decoration: const BoxDecoration(
-                  color: backgroundColor,
-              ),
+              height: MediaQuery.of(context).size.height * 0.33,
+              color: backgroundColor,
+              child: LayoutBuilder(builder: (context, constraints){
+                double innerHeight = constraints.maxHeight;
+                return Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        height: innerHeight * 0.7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: secondaryBackgroundColor,
+                        ),
+                        child: Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 8.0),
+                              child: Row(
+                                mainAxisAlignment : MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text('   0 Stars',style: TextStyle(color: textColor1),),
+                                  user.uid == FirebaseAuth.instance.currentUser!.uid ? TextButton.icon(
+                                      onPressed: (){},
+                                      icon: const Icon(Icons.edit,color: buttonColor2,),
+                                      label: const Text('Edit Profile',style: TextStyle(color: buttonColor2),)
+                                  )
+                                  :
+                                  TextButton.icon(
+                                      onPressed: (){},
+                                      icon: const Icon(Icons.add,color: buttonColor2,),
+                                      label: const Text('Follow',style: TextStyle(color: buttonColor2),)
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 20,),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: [
+                                  Text(user.displayName,
+                                  style: const TextStyle(fontSize: 30),
+                                  ),
+                                  Text(user.email),
+                                ],
+                              ),
+                            ),
+                           Divider(
+                             thickness: 2,
+                             color: containerColor,
+                           ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              mainAxisSize: MainAxisSize.max,
+                              children: [
+                                buildCountColumn('Post', 0),
+                                Container(
+                                  height: 50,
+                                  width: 5,
+                                  color: containerColor,
+                                ),
+                                buildCountColumn('Followers', 0),
+                                Container(
+                                  height: 50,
+                                  width: 5,
+                                  color: containerColor,
+                                ),
+                                buildCountColumn('Following', 0),
+                              ],
+                            )
+                          ],
+                        ),
+
+                      ),
+                    ),
+                    Positioned(
+                      top: 20,
+                      left: 0,
+                      right: 0,
+                      child: Center(
+                        child: CircleAvatar(
+                          backgroundColor: containerColor,
+                          radius: 70,
+                          backgroundImage: CachedNetworkImageProvider(user.photoURL),
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              }),
             ),
-            Expanded(
-              child: Container(
-                decoration: const BoxDecoration(
-                    color: secondaryBackgroundColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(100),
-                    topRight: Radius.circular(100)
-                  )
-                ),
+            const SizedBox(height: 20.0,),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.57,
+              decoration: const BoxDecoration(
+                color: secondaryBackgroundColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  topLeft: Radius.circular(50)
+                )
               ),
             )
           ],
         );
-        // return Padding(
-        //   padding: const EdgeInsets.all(16.0),
-        //   child: Column(
-        //     children: [
-        //       Row(
-        //         children: [
-        //           CircleAvatar(
-        //             radius: 40.0,
-        //             backgroundImage: CachedNetworkImageProvider(user.photoURL),
-        //           ),
-        //           Expanded(
-        //             flex: 1,
-        //               child: Column(
-        //                 children: [
-        //                   Row(
-        //                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //                     mainAxisSize: MainAxisSize.max,
-        //                     children: [
-        //                       buildCountColumn('Post', 0),
-        //                       buildCountColumn('Followers', 0),
-        //                       buildCountColumn('Following', 0),
-        //                     ],
-        //                   ),
-        //                   Row(
-        //                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //                     children: [
-        //                       buildProfileButton(),
-        //                     ],
-        //                   )
-        //                 ],
-        //               )
-        //           )
-        //         ],
-        //       ),
-        //       Container(
-        //         alignment: Alignment.centerLeft,
-        //         padding: EdgeInsets.only(top: 12.0),
-        //         child: Text(user.displayName),
-        //       )
-        //     ],
-        //   ),
-        // );
         }
     );
   }
@@ -143,7 +164,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: secondaryBackgroundColor,
+        backgroundColor: backgroundColor,
         elevation: 0.0,
       ),
       body: profileHeader()
